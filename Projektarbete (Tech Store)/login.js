e9c13e6266a5062c891c2aca6bc943b3da5ee604
juliamattson <julia.mattson@medieinstitutet.se>
2019-11-01 11:24:35 +0100

    no40-skapanyanvändare
    Lagt till skapa-användare-formulär samt funktion som sparar skapad användare i en lista i localstorage

--- Projektarbete (Tech Store)/login.js
+++ Projektarbete (Tech Store)/login.js
@@ -9,3 +9,57 @@ function showNumbers() {
     }
       
 }  
+    var userName = document.getElementById("username").value 
+    var password = document.getElementById("password").value 
+
+    /* Funktion för att logga in om användaren redan är skapad och finns i localstorage */    
+    function logIn() {
+        
+    }
+    
+    /* Funktion för att skapa ny användare och spara till en lista i localstorage över användare */
+    function createUser() { 
+    var username = document.getElementById("createUser").value 
+    var password = document.getElementById("createPassword").value 
+
+    var user = {
+        userName: username,
+        password: password,
+        orders: []
+    }
+
+    var userList = JSON.parse(localStorage.getItem("userList"))
+
+    if(userList) {
+        userList.push(user)
+    } else {
+        userList = []
+        userList.push(user)
+    } 
+
+        console.log(userList)
+    localStorage.setItem("userList", JSON.stringify(userList))
+}
+createUser()
+
+/* var users = [
+    {
+        userName: "",
+        password: "",
+        orders: [
+            {
+                date: "14/03/20", 
+                products: [
+                    {
+
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+
+var loggedInUser = {
+
+} */
